8a7580d5596aefd2af2d6010b34ed674d105975a
meijiasong <1913549290@gmail.com>
2017-11-04 06:55:15 +0000

    bannerSeed

--- database/seeds/BannersTableSeeder.php
+++ database/seeds/BannersTableSeeder.php
@@ -36,7 +36,7 @@ class BannersTableSeeder extends Seeder
            4 => [
                'id' => 5,
                'category_id' => 4,
-               'image' => 'upload/webindex/image/ff206efe7e11de258522df682040c0f0.jpegupload/webindex/image/ff206efe7e11de258522df682040c0f0.jpeg',
+               'image' => 'upload/webindex/image/ff206efe7e11de258522df682040c0f0.jpeg',
            ],
            5 => [
                'id' => 6,
