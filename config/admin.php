c9e68961445098367e35c2b294355fc4b27678b5
meijiasong <1913549290@gmail.com>
2017-11-18 08:50:27 +0000

    zh

--- config/admin.php
+++ config/admin.php
@@ -5,17 +5,17 @@ return [
     /*
      * Laravel-admin name.
      */
-    'name'      => 'Laravel-admin',
+    'name'      => '一树企业',
 
     /*
      * Logo in admin panel header.
      */
-    'logo'      => '<b>Laravel</b> admin',
+    'logo'      => '<b>一树</b> 企业',
 
     /*
      * Mini-logo in admin panel header.
      */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>一树</b>',
 
     /*
      * Route configration.
--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -29,9 +29,9 @@
             </div>
             <div class="other-nav logo-btn-box">
                 <div class="logo-a">
-                    <a class="logo-a-en  {{ preg_match('/^http:\/\/en.isudesign/', request()->url()) ? 'selected' : 'unselected'}}" href="http://en.isudesign.mandokg.com">EN</a>
+                    <a class="logo-a-en  {{ preg_match('/^http:\/\/en.isudesign/', request()->url()) ? 'selected' : 'unselected'}}" href="http://en.isudesign.com">EN</a>
                     <span> / </span>
-                    <a class="cn {{ preg_match('/^http:\/\/en.isudesign/', request()->url()) ? 'unselected': 'selected' }}" href="http://isudesign.mandokg.com">中文</a>
+                    <a class="cn {{ preg_match('/^http:\/\/en.isudesign/', request()->url()) ? 'unselected': 'selected' }}" href="http://www.isudesign.com">中文</a>
                 </div>
                 <div id="btnLogin" hidefocus="true" class="login-btn">
                     <div><img src="image/sanheng.png" class="open"/></div>
